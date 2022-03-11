1868ea2f5b07f145810a7d2f4da508b121ada935
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2022-03-11 19:05:08 +0300

    its not s bug :smiley:

--- src/components/blog/blog_card.jsx
+++ src/components/blog/blog_card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { MdArrowForward, MdRemove } from "react-icons/md";
+import { MdArrowForward, MdMaximize } from "react-icons/md";
 
 function BlogCard({image,title,ltitle,text}) {
     return (
         <div className="card col-sm border-0" >
             <img src={image} className="w-75"  alt="" />
             <div className="row mt-3">
-                < MdRemove  className="font-weight-bold h2 m-2 mt-0 text-warning" />
+                < MdMaximize  className="font-weight-bold h2 m-2 mt-0 text-warning" />
                 <h5 className="text-dark font-weight-bold mb-4 "> {title} <br /> {ltitle}  </h5>
             </div>
            <button className="btn" > {text} < MdArrowForward /> </button>
