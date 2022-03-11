ec9cc60c5464f3f3c7889de42ab5115ef32e1d31
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2022-03-11 21:30:02 +0300

    links fix on the footer :tada:

--- src/components/footer/components/links_card.jsx
+++ src/components/footer/components/links_card.jsx
@@ -4,7 +4,11 @@ function LinksCard({title,links}) {
     return (
         <div className="col-sm ">
             <h4 className=" row font-weight-bold " > {title} </h4>
-            { links.map((link)=>  <span className="row mt-3 mb-3 " > {link} <br /> </span>  ) }
+            { links.map((link)=> 
+                <span className="row mt-3 mb-3 " > 
+                    <a href="#!" className="list-group-item-action text-white" > {link}  </a>                
+                </span>  
+            ) }
             
         </div>
     );
